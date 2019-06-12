2f23c36efdf238fbf757e6016f2e3a5b9e553a35
Tomosugi Tasaka <ttasaka@yahoo-corp.jp>
2019-06-12 11:54:24 +0900

    encrypt Constants.kt

--- app/src/main/java/com/github/tuvy22686/mobiledeveloperchallenge/util/Constants.kt
+++
@@ -1,8 +0,0 @@
-package com.github.tuvy22686.mobiledeveloperchallenge.util
-
-class Constants {
-
-    companion object {
-        const val API_KEY: String = "393d7e91eea017841fc9bf9fe784e94f"
-    }
-}
